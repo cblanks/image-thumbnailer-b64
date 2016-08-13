fc3451ba0270657e5d796d948784c3675a28a142
Christopher Blanks <chris.blanks@gmail.com>
2016-08-13 10:14:37 +0100

    Update image2b64.js

--- image2b64.js
+++ image2b64.js
@@ -1,30 +1,25 @@
 /*
- * Convert an image into base64 thumbnail data for display in HTML, e.g. 
- * <img src="data:image/png;base64,'base64 string goes here'">
+ * Convert an image into base64 PNG-format thumbnail data for display in HTML.
  *
- * To quickly test the output, just copy the output file contents into the 
- * Google Chrome address bar, to see an auto-generated <img> tag.
- *
- * Christopher.Blanks@HPE.com
  */
 
-/*
- * d e p e n d e n c i e s
- */
+//- - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  d e p e n d e n c i e s
+//- - - - - - - - - - - - - - - - - - - - - - - - - - -
 var child = require('child_process'),
 	fs = require('fs'),
 	path = require('path'),
 	process = require('process');
 
-/*
- * v a r i a b l e s
- */
+//- - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  v a r i a b l e s
+//- - - - - - - - - - - - - - - - - - - - - - - - - - -
 var thumbSize = 56, // pixels
 	outputFile = 'b64str.txt';
 
-/*
- * m a i n
- */
+//- - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  m a i n
+//- - - - - - - - - - - - - - - - - - - - - - - - - - -
 try {
 	var imagePath = path.resolve(process.argv[2]);
 	
@@ -34,13 +29,15 @@ try {
 }
 
 var pathParts = path.parse(imagePath),
-	thumbPath = './'+pathParts.name+'-'+thumbSize.toString()+'.png',
+	thumbPath = path.resolve(
+		pathParts.name+'_'+thumbSize.toString()+'.png'
+	),
 	cmd = 'convert '+imagePath+' -thumbnail '+thumbSize+' '+thumbPath;
  
 child.exec(cmd, function(error, stdout, stderr) {
 	
 	fs.readFile(thumbPath, {encoding: 'base64'}, function(err, base64string) {
 		
-		fs.writeFileSync(outputFile, 'data:image/png;base64,'+base64string);
+		fs.writeFileSync(outputFile, '<img src=data:image/png;base64,'+base64string+' alt="'+pathParts.name+'"/>');
 	});
 });
